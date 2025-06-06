8361f48ddf6a568408582a4a55a4374c3b57540e
JeremySeq <jaysequeir@gmail.com>
2025-06-05 19:55:11 -0500

    Bogre drops Bracer of Might

--- src/main/java/com/jeremyseq/inhabitants/entities/bogre/BogreEntity.java
+++ src/main/java/com/jeremyseq/inhabitants/entities/bogre/BogreEntity.java
@@ -13,6 +13,8 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -365,6 +367,12 @@ public class BogreEntity extends Monster implements GeoEntity {
     @Override
     public float getStepHeight() {
         return 1.5f;
+    }
+
+    @Override
+    protected void dropCustomDeathLoot(DamageSource source, int looting, boolean recentlyHit) {
+        super.dropCustomDeathLoot(source, looting, recentlyHit);
+        this.spawnAtLocation(new ItemStack(ModItems.BRACER_OF_MIGHT.get()));
     }
 
     @Override
