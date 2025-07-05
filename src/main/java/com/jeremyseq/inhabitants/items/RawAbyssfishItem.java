af19e28159f833cd03d23be092e8bfcccf736edd
mattseq <matthewsequeir@gmail.com>
2025-07-04 19:01:36 -0500

    abyssfish/pitfish gives back oxygen when eaten, can always be eaten regardless of hunger

--- src/main/java/com/jeremyseq/inhabitants/items/RawAbyssfishItem.java
+++ src/main/java/com/jeremyseq/inhabitants/items/RawAbyssfishItem.java
@@ -1,7 +1,17 @@
 package com.jeremyseq.inhabitants.items;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class RawAbyssfishItem extends Item {
     public RawAbyssfishItem() {
@@ -12,5 +22,23 @@ public class RawAbyssfishItem extends Item {
             .nutrition(2)
             .saturationMod(0.1F)
             .meat()
+            .alwaysEat()
             .build();
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
+        if (!pLevel.isClientSide && pLivingEntity instanceof Player player) {
+            // Add oxygen
+            int maxAir = player.getMaxAirSupply(); // usually 300
+            int currentAir = player.getAirSupply();
+            player.setAirSupply(Math.min(currentAir + 100, maxAir));
+        }
+        return super.finishUsingItem(pStack, pLevel, pLivingEntity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(Component.literal("§bGrants extra oxygen when eaten!").withStyle(ChatFormatting.AQUA));
+        tooltip.add(Component.literal("A delicacy from the deep abyss...").withStyle(ChatFormatting.DARK_PURPLE));
+    }
 }
