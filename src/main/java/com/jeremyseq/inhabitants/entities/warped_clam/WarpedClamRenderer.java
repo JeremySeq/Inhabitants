5905afa2f7d76ae03720aa247052b118ca4ccef1
JeremySeq <jaysequeir@gmail.com>
2025-07-06 20:09:28 -0500

    updated warped clam model and added glowmask

--- src/main/java/com/jeremyseq/inhabitants/entities/warped_clam/WarpedClamRenderer.java
+++ src/main/java/com/jeremyseq/inhabitants/entities/warped_clam/WarpedClamRenderer.java
@@ -5,11 +5,13 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class WarpedClamRenderer extends GeoEntityRenderer<WarpedClamEntity> {
     public WarpedClamRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new WarpedClamModel());
         this.shadowRadius = 0.5f;
+        addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 
     @Override
