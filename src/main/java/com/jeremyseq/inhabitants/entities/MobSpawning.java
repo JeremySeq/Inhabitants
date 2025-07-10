4ed10aeacdbcb3027f5e270407f8fa0214c7154e
JeremySeq <jaysequeir@gmail.com>
2025-07-09 22:11:26 -0500

    added impaler spawning

--- src/main/java/com/jeremyseq/inhabitants/entities/MobSpawning.java
+++ src/main/java/com/jeremyseq/inhabitants/entities/MobSpawning.java
@@ -3,6 +3,7 @@ package com.jeremyseq.inhabitants.entities;
 import com.jeremyseq.inhabitants.Inhabitants;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,6 +25,13 @@ public class MobSpawning {
                 SpawnPlacements.Type.IN_WATER,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 (type, level, reason, pos, random) -> level.getBlockState(pos).getFluidState().is(FluidTags.WATER) && pos.getY() < 45,
+                SpawnPlacementRegisterEvent.Operation.REPLACE
+        );
+        event.register(
+                ModEntities.IMPALER.get(),
+                SpawnPlacements.Type.ON_GROUND,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                (type, level, reason, pos, random) -> pos.getY() < 45 && level.getBrightness(LightLayer.BLOCK, pos) < 8,
                 SpawnPlacementRegisterEvent.Operation.REPLACE
         );
 
