eb28b4582b0360ac15b47be2ef4532ae98a13426
mattseq <matthewsequeir@gmail.com>
2025-09-22 19:53:26 -0500

    made gazer pod block drop a gazer pod item (w/ corresponding HasGazer nbt) if mined with silk touch pick, destroy progress happens at the right speed

--- src/main/java/com/jeremyseq/inhabitants/blocks/ModBlocks.java
+++ src/main/java/com/jeremyseq/inhabitants/blocks/ModBlocks.java
@@ -23,7 +23,7 @@ public class ModBlocks
     );
     public static final RegistryObject<Block> GAZER_POD_BLOCK = registerBlock(
             "gazer_pod_block",
-            () -> new GazerPodBlock(Block.Properties.copy(Blocks.NETHERRACK).dynamicShape().noOcclusion())
+            () -> new GazerPodBlock(Block.Properties.copy(Blocks.NETHERRACK).requiresCorrectToolForDrops().noOcclusion())
     );
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
