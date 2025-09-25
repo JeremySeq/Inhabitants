e4685b5c1dba52b64b13e612ed11da3c5bca5898
mattseq <matthewsequeir@gmail.com>
2025-09-25 18:20:21 -0500

    added gazer bone to GazerPodBlock model and texture, and shows bone if block has gazer

--- src/main/java/com/jeremyseq/inhabitants/blocks/entity/client/GazerPodBlockModel.java
+++ src/main/java/com/jeremyseq/inhabitants/blocks/entity/client/GazerPodBlockModel.java
@@ -3,11 +3,16 @@ package com.jeremyseq.inhabitants.blocks.entity.client;
 import com.jeremyseq.inhabitants.Inhabitants;
 import com.jeremyseq.inhabitants.blocks.entity.GazerPodBlockEntity;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
+import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
 
 public class GazerPodBlockModel extends GeoModel<GazerPodBlockEntity> {
+    private GazerPodBlockEntity modelEntity;
+
     @Override
     public ResourceLocation getModelResource(GazerPodBlockEntity gazerPodBlockEntity) {
+        this.modelEntity = gazerPodBlockEntity;
         return ResourceLocation.fromNamespaceAndPath(Inhabitants.MODID, "geo/gazer_pod_block.geo.json");
     }
 
@@ -19,5 +24,14 @@ public class GazerPodBlockModel extends GeoModel<GazerPodBlockEntity> {
     @Override
     public ResourceLocation getAnimationResource(GazerPodBlockEntity gazerPodBlockEntity) {
         return null;
+    }
+
+    @Override
+    public void setCustomAnimations(GazerPodBlockEntity animatable, long instanceId, AnimationState<GazerPodBlockEntity> animationState) {
+        super.setCustomAnimations(animatable, instanceId, animationState);
+        CoreGeoBone gazerBone = this.getAnimationProcessor().getBone("gazer");
+        if (gazerBone != null) {
+            gazerBone.setHidden(!animatable.hasGazer());
+        }
     }
 }
