d6746c231f76278e4ce1022ded41623dfad2441e
mattseq <matthewsequeir@gmail.com>
2025-10-10 18:39:17 -0500

    catcher uses a different texture of waterberry bush, better shape/hitbox for bush

--- src/main/java/com/jeremyseq/inhabitants/blocks/WaterberryBushBlock.java
+++ src/main/java/com/jeremyseq/inhabitants/blocks/WaterberryBushBlock.java
@@ -12,15 +12,27 @@ import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class WaterberryBushBlock extends Block {
+    public static final BooleanProperty FAKE = BooleanProperty.create("fake");
+
     public WaterberryBushBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FAKE, false));
+    }
+
+    @Override
+    public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter world, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        return Block.box(4, 0, 4, 12, 16, 12);
     }
 
     @Override
@@ -48,5 +60,10 @@ public class WaterberryBushBlock extends Block {
     @Override
     public @NotNull List<ItemStack> getDrops(@NotNull BlockState pState, LootParams.@NotNull Builder pParams) {
         return List.of(new ItemStack(ModItems.WATERBERRY_ITEM.get(), 1));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FAKE);
     }
 }
