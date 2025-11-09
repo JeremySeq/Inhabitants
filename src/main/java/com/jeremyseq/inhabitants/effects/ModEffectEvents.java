004a458c421f96b81c4b441b29deb7cf0707f810
mattseq <matthewsequeir@gmail.com>
2025-11-08 21:33:02 -0600

    if the entity has immunity effect, damage taken by magic and other similar damage sources is negated

--- src/main/java/com/jeremyseq/inhabitants/effects/ModEffectEvents.java
+++ src/main/java/com/jeremyseq/inhabitants/effects/ModEffectEvents.java
@@ -3,6 +3,7 @@ package com.jeremyseq.inhabitants.effects;
 import com.jeremyseq.inhabitants.Inhabitants;
 import com.jeremyseq.inhabitants.items.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -10,6 +11,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -30,6 +32,17 @@ public class ModEffectEvents {
                 if (!effect.getEffect().equals(ModEffects.IMMUNITY.get())) {
                     entity.removeEffect(effect.getEffect());
                 }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLivingHurt(LivingHurtEvent event) {
+        LivingEntity entity = event.getEntity();
+
+        if (entity.hasEffect(ModEffects.IMMUNITY.get())) {
+            if (event.getSource().type().equals(DamageTypes.MAGIC) || event.getSource().type().equals(DamageTypes.WITHER) || event.getSource().type().equals(DamageTypes.DRAGON_BREATH) || event.getSource().is(DamageTypes.INDIRECT_MAGIC)) {
+                event.setCanceled(true);
             }
         }
     }
