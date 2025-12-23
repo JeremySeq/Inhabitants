3e0e0c381652f86f1fcefd595ac9b97392155e28
mattseq <matthewsequeir@gmail.com>
2025-12-22 20:35:11 -0600

    warped clam doesnt glow if it doesnt have a pearl

--- src/main/java/com/jeremyseq/inhabitants/entities/warped_clam/WarpedClamRenderer.java
+++ src/main/java/com/jeremyseq/inhabitants/entities/warped_clam/WarpedClamRenderer.java
@@ -1,9 +1,14 @@
 package com.jeremyseq.inhabitants.entities.warped_clam;
 
 import com.jeremyseq.inhabitants.Inhabitants;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
@@ -11,7 +16,15 @@ public class WarpedClamRenderer extends GeoEntityRenderer<WarpedClamEntity> {
     public WarpedClamRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new WarpedClamModel());
         this.shadowRadius = 0.5f;
-        addRenderLayer(new AutoGlowingGeoLayer<>(this));
+        addRenderLayer(new AutoGlowingGeoLayer<>(this) {
+            @Override
+            public void render(PoseStack poseStack, WarpedClamEntity animatable, BakedGeoModel bakedModel, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
+                if (!animatable.hasPearl()) {
+                    return;
+                }
+                super.render(poseStack, animatable, bakedModel, renderType, bufferSource, buffer, partialTick, packedLight, packedOverlay);
+            }
+        });
     }
 
     @Override
