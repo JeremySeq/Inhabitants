f549abe2c337bfbdd4d7933c2cd5b86b1a03b4c4
mattseq <matthewsequeir@gmail.com>
2025-12-26 13:46:21 -0600

    FishSnotChowderItem doesnt give nausea if player is in water

--- src/main/java/com/jeremyseq/inhabitants/items/FishSnotChowderItem.java
+++ src/main/java/com/jeremyseq/inhabitants/items/FishSnotChowderItem.java
@@ -4,12 +4,14 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BowlFoodItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -20,8 +22,16 @@ public class FishSnotChowderItem extends BowlFoodItem {
                 new FoodProperties.Builder()
                         .nutrition(10)
                         .saturationMod(0.8f)
-                        .effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 300), 1.0f)
                         .build()));
+    }
+
+    @Override
+    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack pStack, @NotNull Level pLevel, LivingEntity pEntityLiving) {
+        if (!pEntityLiving.isInFluidType(Fluids.WATER.getFluidType())) {
+            pEntityLiving.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 300, 1, false, true, true));
+        }
+
+        return super.finishUsingItem(pStack, pLevel, pEntityLiving);
     }
 
     @Override
