5f2cc5ddc253dbb45c6ee011cb650b5a8977bc5c
mattseq <matthewsequeir@gmail.com>
2026-01-19 19:40:46 -0600

    added Bonk and My Precious paintings, including lang

---
+++ src/main/java/com/jeremyseq/inhabitants/paintings/ModPaintings.java
@@ -0,0 +1,23 @@
+package com.jeremyseq.inhabitants.paintings;
+
+import com.jeremyseq.inhabitants.Inhabitants;
+import net.minecraft.world.entity.decoration.PaintingVariant;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModPaintings {
+    public static final DeferredRegister<PaintingVariant> PAINTINGS =
+            DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, Inhabitants.MODID);
+
+    public static final RegistryObject<PaintingVariant> BONK =
+            PAINTINGS.register("bonk", () -> new PaintingVariant(64, 48));
+
+    public static final RegistryObject<PaintingVariant> MY_PRECIOUS =
+            PAINTINGS.register("my_precious", () -> new PaintingVariant(48, 48));
+
+    public static void register(IEventBus eventBus) {
+        PAINTINGS.register(eventBus);
+    }
+}
