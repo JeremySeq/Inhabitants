ed1342bd3dd157669503502005d10219aed567e0
JeremySeq <jaysequeir@gmail.com>
2026-02-24 19:02:42 -0600

    impaler: concussion subtitle bug fixes

--- src/main/java/com/jeremyseq/inhabitants/mixin/SubtitleOverlayMixin.java
+++ src/main/java/com/jeremyseq/inhabitants/mixin/SubtitleOverlayMixin.java
@@ -7,21 +7,33 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 /**
- * Mixin to change all subtitle texts to "???" when the player has the concussion effect.
+ * Mixin to obscure all subtitle texts when the player has the concussion effect.
  */
-@Mixin(SubtitleOverlay.Subtitle.class)
+@Mixin(SubtitleOverlay.class)
 public abstract class SubtitleOverlayMixin {
 
-    @Inject(method="getText", at=@At("HEAD"), cancellable = true)
-    private void getText(CallbackInfoReturnable<Component> cir) {
-        if (Minecraft.getInstance().player == null) return;
-        MobEffectInstance concussion_effect = Minecraft.getInstance().player.getEffect(ModEffects.CONCUSSION.get());
-        if (concussion_effect != null) {
-            cir.setReturnValue(Component.literal("???"));
-        }
+    /**
+     * replaces the rendered subtitle text with inaudible without modifying the stored subtitle text (prevents spam)
+     */
+    @ModifyVariable(
+            method = "render",
+            at = @At(
+                    value = "INVOKE_ASSIGN",
+                    target = "Lnet/minecraft/client/gui/components/SubtitleOverlay$Subtitle;getText()Lnet/minecraft/network/chat/Component;"
+            ),
+            ordinal = 0
+    )
+    private Component inhabitants$modifyRenderedSubtitle(Component original) {
+
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.player == null) return original;
+
+        MobEffectInstance effect = mc.player.getEffect(ModEffects.CONCUSSION.get());
+        if (effect == null) return original;
+
+        return Component.translatable("subtitles.inhabitants.inaudible");
     }
 }
