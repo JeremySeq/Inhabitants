ab2c4976c40980fd3ee6db3a931bf4f1ae662e29
JeremySeq <jaysequeir@gmail.com>
2026-03-17 18:33:27 -0500

    added paintings to creative mode tab

--- src/main/java/com/jeremyseq/inhabitants/items/ModCreativeModeTabs.java
+++ src/main/java/com/jeremyseq/inhabitants/items/ModCreativeModeTabs.java
@@ -1,10 +1,14 @@
 package com.jeremyseq.inhabitants.items;
 
 import com.jeremyseq.inhabitants.Inhabitants;
+import com.jeremyseq.inhabitants.paintings.ModPaintings;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.decoration.PaintingVariant;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -29,8 +33,19 @@ public class ModCreativeModeTabs {
                         pOutput.accept(ModItems.WARPED_CLAM_ITEM.get());
                         pOutput.accept(ModItems.IMPALER_SPAWN_EGG.get());
                         pOutput.accept(ModItems.IMPALER_SPIKE.get());
+                        pOutput.accept(paintingStack(ModPaintings.ENDERMANS_LAST_DAY));
+                        pOutput.accept(paintingStack(ModPaintings.MY_PRECIOUS));
                     })
                     .build());
+
+    private static ItemStack paintingStack(RegistryObject<PaintingVariant> variant) {
+        ItemStack stack = new ItemStack(Items.PAINTING);
+        CompoundTag entityTag = new CompoundTag();
+        assert variant.getId() != null;
+        entityTag.putString("variant", variant.getId().toString());
+        stack.getOrCreateTag().put("EntityTag", entityTag);
+        return stack;
+    }
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
