a9e2d63dddea850717bd8a2038a6a650459cc0ea
mattseq <matthewsequeir@gmail.com>
2026-04-10 21:00:13 -0500

    nightmare: animations, goals, flying, drops (placeholder drop)

--- src/main/java/com/jeremyseq/inhabitants/entities/nightmare/NightmareEntity.java
+++ src/main/java/com/jeremyseq/inhabitants/entities/nightmare/NightmareEntity.java
@@ -1,34 +1,115 @@
 package com.jeremyseq.inhabitants.entities.nightmare;
 
+import com.jeremyseq.inhabitants.items.ModItems;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.control.FlyingMoveControl;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomFlyingGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
+import software.bernie.geckolib.core.object.PlayState;
+
+import java.util.Random;
 
 public class NightmareEntity extends Monster implements GeoEntity {
     private final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
 
+    public static final int MAX_DYING_TICKS = 120;
+
+    public int dyingTicks = 0;
+
     public NightmareEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+        this.moveControl = new FlyingMoveControl(this, 20, true);
+    }
+
+    protected @NotNull PathNavigation createNavigation(@NotNull Level pLevel) {
+        FlyingPathNavigation flyingpathnavigation = new FlyingPathNavigation(this, pLevel);
+        flyingpathnavigation.setCanOpenDoors(false);
+        flyingpathnavigation.setCanFloat(true);
+        flyingpathnavigation.setCanPassDoors(true);
+        return flyingpathnavigation;
     }
 
     public static AttributeSupplier setAttributes() {
         return Monster.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 30f)
-                .add(Attributes.ATTACK_DAMAGE, 8f)
+                .add(Attributes.ATTACK_DAMAGE, 10f)
                 .add(Attributes.ATTACK_SPEED, 1.0f)
                 .add(Attributes.ATTACK_KNOCKBACK, 1.5F)
                 .add(Attributes.FOLLOW_RANGE, 30f)
-                .add(Attributes.MOVEMENT_SPEED, .25f).build();
+                .add(Attributes.FLYING_SPEED, .3f).build();
+    }
+
+    @Override
+    public boolean doHurtTarget(@NotNull Entity pEntity) {
+        if (!level().isClientSide) {
+            this.triggerAnim("attack", "melee");
+        }
+        return super.doHurtTarget(pEntity);
+    }
+
+    @Override
+    public void die(@NotNull DamageSource pDamageSource) {
+        this.triggerAnim("death", "death");
+        this.dead = true;
+    }
+
+    @Override
+    public void tickDeath() {
+        ++this.dyingTicks;
+        if (this.dyingTicks >= MAX_DYING_TICKS) {
+            this.remove(Entity.RemovalReason.KILLED);
+            this.playSound(SoundEvents.ITEM_PICKUP);
+            this.spawnAtLocation(new ItemStack(ModItems.NIGHTMARE_SPAWN_EGG.get(), new Random().nextInt(1, 3)));
+        }
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+        controllers.add(new AnimationController<>(this, "movement", 10, this::predicate));
+        controllers.add(new AnimationController<>(this, "attack", 0, state -> PlayState.STOP)
+                .triggerableAnim("melee", RawAnimation.begin().then("melee", Animation.LoopType.PLAY_ONCE)));
+        controllers.add(new AnimationController<>(this, "death", 0, state -> PlayState.STOP)
+                .triggerableAnim("death", RawAnimation.begin().then("death", Animation.LoopType.HOLD_ON_LAST_FRAME)));
+    }
+
+    private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> animationState) {
+        if (animationState.isMoving()) {
+            animationState.getController().setAnimation(RawAnimation.begin().then("flying", Animation.LoopType.LOOP));
+        } else {
+            animationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
+        }
+
+        return PlayState.CONTINUE;
+    }
+
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, true));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this , Player.class, false));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomFlyingGoal(this, 1.0D));
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
     }
 
     @Override
