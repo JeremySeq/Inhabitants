f60c554025acc12171a6de1b0335c4e88db68b56
BoboTweaks <bobotweaks@gmail.com>
2026-05-09 03:58:18 +0300

    Concher: Spawns in Warm ocean biome

--- src/main/java/com/jeremyseq/inhabitants/entities/MobSpawning.java
+++ src/main/java/com/jeremyseq/inhabitants/entities/MobSpawning.java
@@ -11,6 +11,7 @@ import net.minecraft.world.level.levelgen.FlatLevelSource;
 import net.minecraft.world.level.storage.WorldData;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.util.RandomSource;
+import net.minecraft.tags.FluidTags;
 
 import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,24 +22,30 @@ public class MobSpawning {
     @SubscribeEvent
     public static void entitySpawnRestriction(SpawnPlacementRegisterEvent event) {
         event.register(
-            ModEntities.WARPED_CLAM.get(),
-            SpawnPlacements.Type.ON_GROUND,
-            Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-            (entityType, level, spawnType, pos, random) -> true,
-            SpawnPlacementRegisterEvent.Operation.REPLACE
-        );
+                ModEntities.WARPED_CLAM.get(),
+                SpawnPlacements.Type.ON_GROUND,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                (entityType, level, spawnType, pos, random) -> true,
+                SpawnPlacementRegisterEvent.Operation.REPLACE);
         event.register(
-            ModEntities.IMPALER.get(),
-            SpawnPlacements.Type.ON_GROUND,
-            Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                (type, level, reason, pos, random) -> pos.getY() < 45 
-                    && level.getBrightness(LightLayer.BLOCK, pos) < 8
-                    && (isFlat(level) ? level.getMaxLocalRawBrightness(pos) < 8 : true)
-                    && spawnChance(level, random, "impaler")
-                    && !level.getBiome(pos).is(Biomes.DEEP_DARK)
-                    && !level.getBiome(pos).is(Biomes.MUSHROOM_FIELDS),
-                SpawnPlacementRegisterEvent.Operation.REPLACE
-        );
+                ModEntities.IMPALER.get(),
+                SpawnPlacements.Type.ON_GROUND,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                (type, level, reason, pos, random) -> pos.getY() < 45
+                        && level.getBrightness(LightLayer.BLOCK, pos) < 8
+                        && (isFlat(level) ? level.getMaxLocalRawBrightness(pos) < 8 : true)
+                        && spawnChance(level, random, "impaler")
+                        && !level.getBiome(pos).is(Biomes.DEEP_DARK)
+                        && !level.getBiome(pos).is(Biomes.MUSHROOM_FIELDS),
+                SpawnPlacementRegisterEvent.Operation.REPLACE);
+
+        event.register(
+                ModEntities.CONCHER.get(),
+                SpawnPlacements.Type.IN_WATER,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                (type, level, reason, pos, random) -> level.getBiome(pos).is(Biomes.WARM_OCEAN)
+                        && level.getFluidState(pos).is(FluidTags.WATER),
+                SpawnPlacementRegisterEvent.Operation.REPLACE);
 
     }
 
@@ -47,14 +54,14 @@ public class MobSpawning {
             return true;
         }
         return level.getChunkSource() instanceof ServerChunkCache scc &&
-            scc.getGenerator() instanceof FlatLevelSource;
+                scc.getGenerator() instanceof FlatLevelSource;
     }
 
     // can be used in the future for new mobs :P
     private static boolean spawnChance(LevelAccessor level, RandomSource random, String mob) {
         if ("impaler".equals(mob)) {
             if (isFlat(level)) {
-                return random.nextFloat() < 0.5f; //50%
+                return random.nextFloat() < 0.5f; // 50%
             }
             return random.nextFloat() < 0.95f; // 95%
         }
