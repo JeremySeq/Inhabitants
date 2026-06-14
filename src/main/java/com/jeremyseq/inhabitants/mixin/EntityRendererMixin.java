4c3c8188b705289281cb561003899e8666f751d2
mattseq <matthewsequeir@gmail.com>
2026-06-14 09:46:34 -0500

    fix(impaler): remove unused texture variable, wrap rendering and unsafe casting in try-finally blocks fixes #25

--- src/main/java/com/jeremyseq/inhabitants/mixin/EntityRendererMixin.java
+++ src/main/java/com/jeremyseq/inhabitants/mixin/EntityRendererMixin.java
@@ -10,8 +10,6 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.util.Mth;
 import net.minecraft.network.chat.Component;
 
@@ -52,52 +50,53 @@ public abstract class EntityRendererMixin<T extends Entity> {
             if (history == null || history.isEmpty()) return;
 
             inhabitants$isGhostRendering = true;
-            
-            ResourceLocation texture = ((EntityRenderer<T>)(Object)this).getTextureLocation(pEntity);
-            RenderType ghostType = RenderType.entityTranslucent(texture);
-            
-            double curX = Mth.lerp(pPartialTick, pEntity.xo, pEntity.getX());
-            double curY = Mth.lerp(pPartialTick, pEntity.yo, pEntity.getY());
-            double curZ = Mth.lerp(pPartialTick, pEntity.zo, pEntity.getZ());
-            
-            int renderedCount = 0;
-            for (int i = 1; i < history.size() && renderedCount < 2; i += 2) {
-                GhostState state = history.get(i);
-                renderedCount++;
-                
-                pMatrixStack.pushPose();
-                
-                pMatrixStack.translate(
-                    state.pos().x - curX,
-                    state.pos().y - curY,
-                    state.pos().z - curZ
-                );
-                
-                float deltaYaw = state.camYaw() - mc.player.getYRot();
-                float deltaPitch = state.camPitch() - mc.player.getXRot();
-                
-                while (deltaYaw < -180.0F) deltaYaw += 360.0F;
-                while (deltaYaw >= 180.0F) deltaYaw -= 360.0F;
-                
-                float rotationLagFactor = 0.05F * ModClientEvents.muffleLerp;
-                pMatrixStack.translate(deltaYaw * rotationLagFactor, -deltaPitch * rotationLagFactor, 0);
-                
-                float alphaBase = ModClientEvents.muffleLerp * 0.2F;
-                float fade = 1.0F - ((float)i / history.size());
-                float alpha = alphaBase * fade;
-                
-                MultiBufferSource ghostSource = new GhostRenderUtils.GhostBufferSource(pBuffer, alpha);
 
-                EntityRenderer<T> renderer = (EntityRenderer<T>) (Object) this;
+            try {
+                double curX = Mth.lerp(pPartialTick, pEntity.xo, pEntity.getX());
+                double curY = Mth.lerp(pPartialTick, pEntity.yo, pEntity.getY());
+                double curZ = Mth.lerp(pPartialTick, pEntity.zo, pEntity.getZ());
 
-                renderer.render(pEntity, state.yRot(),
-                    pPartialTick, pMatrixStack,
-                    ghostSource, pPackedLight);
-                
-                pMatrixStack.popPose();
+                int renderedCount = 0;
+                for (int i = 1; i < history.size() && renderedCount < 2; i += 2) {
+                    GhostState state = history.get(i);
+                    renderedCount++;
+
+                    pMatrixStack.pushPose();
+
+                    try {
+                        pMatrixStack.translate(
+                                state.pos().x - curX,
+                                state.pos().y - curY,
+                                state.pos().z - curZ
+                        );
+
+                        float deltaYaw = state.camYaw() - mc.player.getYRot();
+                        float deltaPitch = state.camPitch() - mc.player.getXRot();
+
+                        while (deltaYaw < -180.0F) deltaYaw += 360.0F;
+                        while (deltaYaw >= 180.0F) deltaYaw -= 360.0F;
+
+                        float rotationLagFactor = 0.05F * ModClientEvents.muffleLerp;
+                        pMatrixStack.translate(deltaYaw * rotationLagFactor, -deltaPitch * rotationLagFactor, 0);
+
+                        float alphaBase = ModClientEvents.muffleLerp * 0.2F;
+                        float fade = 1.0F - ((float) i / history.size());
+                        float alpha = alphaBase * fade;
+
+                        MultiBufferSource ghostSource = new GhostRenderUtils.GhostBufferSource(pBuffer, alpha);
+
+                        EntityRenderer<T> renderer = (EntityRenderer<T>) (Object) this;
+
+                        renderer.render(pEntity, state.yRot(),
+                                pPartialTick, pMatrixStack,
+                                ghostSource, pPackedLight);
+                    } finally {
+                        pMatrixStack.popPose();
+                    }
+                }
+            } finally {
+                inhabitants$isGhostRendering = false;
             }
-            
-            inhabitants$isGhostRendering = false;
         }
     }
 
